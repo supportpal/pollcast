7251107d8fe32f8b221b8a68ee66c6ab6c22b526
Kieran Brahney <kieran@supportpal.com>
2021-06-23 19:44:19 +0100

    phpcbf

--- tests/Model/ChannelTest.php
+++ tests/Model/ChannelTest.php
@@ -5,6 +5,8 @@ namespace Tests\Model;
 use SupportPal\Pollcast\Model\Channel;
 use Tests\TestCase;
 
+use function factory;
+
 class ChannelTest extends TestCase
 {
     public function testAccessNameAttribute(): void
