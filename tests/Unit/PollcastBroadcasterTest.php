27dc1a11f37d168c05011c60a3f8132064b973f2
Jay Shah <602425+jshah4517@users.noreply.github.com>
2021-06-25 18:04:25 +0100

    add test for garbage collection

--- tests/Unit/PollcastBroadcasterTest.php
+++ tests/Unit/PollcastBroadcasterTest.php
@@ -3,8 +3,11 @@
 namespace SupportPal\Pollcast\Tests\Unit;
 
 use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Carbon;
 use SupportPal\Pollcast\Broadcasting\Socket;
 use SupportPal\Pollcast\Model\Channel;
+use SupportPal\Pollcast\Model\Member;
+use SupportPal\Pollcast\Model\Message;
 use SupportPal\Pollcast\PollcastBroadcaster;
 use SupportPal\Pollcast\Tests\TestCase;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
@@ -52,7 +55,7 @@ class PollcastBroadcasterTest extends TestCase
     {
         $broadcaster = $this->setupBroadcaster();
 
-        $channelName1 = 'fake-channel';
+        $channelName1 = 'public-channel';
         $channelName2 = 'private-channel';
         $userFunction = function (User $user) {
             return true;
@@ -78,9 +81,12 @@ class PollcastBroadcasterTest extends TestCase
 
     public function testBroadcastWithoutSocket(): void
     {
+        $date = '2021-06-01 12:00:00';
+        Carbon::setTestNow(Carbon::parse($date));
+
         $broadcaster = $this->setupBroadcaster();
 
-        $channelName1 = 'fake-channel';
+        $channelName1 = 'public-channel';
         $channelName2 = 'private-channel';
         $userFunction = function (User $user) {
             return true;
@@ -102,6 +108,53 @@ class PollcastBroadcasterTest extends TestCase
         }
 
         $this->assertDatabaseCount('pollcast_message_queue', 2);
+    }
+
+    public function testBroadcastGarbageCollection(): void
+    {
+        // Update lottery so garbage collection always runs.
+        config(['pollcast.lottery' => [1, 1]]);
+
+        $broadcaster = $this->setupBroadcaster();
+
+        $channelName1 = 'public-channel';
+        $channel1 = factory(Channel::class)->create([
+            'name'       => $channelName1,
+            'updated_at' => Carbon::now()->subDays(2)->toDateTimeString()
+        ]);
+
+        $channelName2 = 'private-channel';
+        $channel2 = factory(Channel::class)->create([
+            'name'       => $channelName2,
+            'updated_at' => Carbon::now()->subDay()->toDateTimeString()
+        ]);
+
+        $member1 = factory(Member::class)->create([
+            'channel_id' => $channel2->id,
+            'updated_at' => Carbon::now()->subSeconds(15)->toDateTimeString(),
+        ]);
+        $member2 = factory(Member::class)->create([
+            'channel_id' => $channel2->id,
+            'updated_at' => Carbon::now()->subSeconds(5)->toDateTimeString(),
+        ]);
+
+        $message1 = factory(Message::class)->create([
+            'channel_id' => $channel2->id,
+            'created_at' => Carbon::now()->subSeconds(5)->toDateTimeString(),
+        ]);
+        $message2 = factory(Message::class)->create([
+            'channel_id' => $channel2->id,
+            'created_at' => Carbon::now()->subSeconds(15)->toDateTimeString(),
+        ]);
+
+        $broadcaster->broadcast([], 'test-event', []);
+
+        $this->assertDatabaseMissing('pollcast_channel', ['id' => $channel1->id]);
+        $this->assertDatabaseHas('pollcast_channel', ['id' => $channel2->id]);
+        $this->assertDatabaseMissing('pollcast_channel_members', ['id' => $member1->id]);
+        $this->assertDatabaseHas('pollcast_channel_members', ['id' => $member2->id]);
+        $this->assertDatabaseHas('pollcast_message_queue', ['id' => $message1->id]);
+        $this->assertDatabaseMissing('pollcast_message_queue', ['id' => $message2->id]);
     }
 
     /**
