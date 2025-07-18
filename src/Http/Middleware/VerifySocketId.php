98e9fdda778b6c088d518fdc842e44101f3e7f59
Jay Shah <602425+jshah4517@users.noreply.github.com>
2025-07-18 13:31:06 +0100

    refactor

--- src/Http/Middleware/VerifySocketId.php
+++ src/Http/Middleware/VerifySocketId.php
@@ -21,14 +21,29 @@ class VerifySocketId
     public function handle(Request $request, Closure $next)
     {
         try {
-            $this->socket->setId(
-                ! empty($request->header(Socket::HEADER)) ? $this->socket->getIdFromRequest()
-                    : $this->socket->getIdFromSession()
-            );
+            $this->socket->setId($this->getSocketId($request));
         } catch (InvalidSocketException $e) {
             throw new UnauthorizedException($e->getMessage());
         }
 
         return $next($request);
+    }
+
+    /**
+     * @throws InvalidSocketException
+     */
+    private function getSocketId(Request $request): string
+    {
+        if (! empty($request->header(Socket::HEADER))) {
+            return $this->socket->getIdFromRequest();
+        }
+
+        $id = $this->socket->getIdFromSession();
+
+        if ($id === null) {
+            throw new InvalidSocketException('Socket ID is not set.');
+        }
+
+        return $id;
     }
 }
